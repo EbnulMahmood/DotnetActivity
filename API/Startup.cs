f8c2018535720a0066654ab8ee1d5f3095511193
EbnulMahmood <shovonmahmood67@gmail.com>
2022-06-10 16:25:05 +0600

    added material-ui

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,8 +33,18 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
-            services.AddDbContext<DataContext>(opt =>{
+            services.AddDbContext<DataContext>(opt =>
+            {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .WithOrigins("http://localhost:3000");
+                });
             });
         }
 
@@ -51,6 +61,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
---
+++ client-app/src/App.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+function App() {
+  const [activities, SetActivities] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities').then(response => {
+      console.log(response);
+      SetActivities(response.data);
+    })
+  }, [])
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+        <Divider />
+        <nav aria-label="main mailbox folders">
+          <List>
+            {activities.map((activity: any) => (
+              <ListItem key={activity.id} disablePadding>
+                <ListItemText primary={activity.title} />
+              </ListItem>
+            ))}
+          </List>
+        </nav>
+      </Box>
+    </div>
+  );
+}
+
+export default App;
